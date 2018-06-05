941814f2619c48693e73e3cf704c7661712f6b4a
missmeow71 <sensuousdesigner@gmail.com>
2018-06-05 15:30:07 -0600

    created react app & added components

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+class Header extends Component {
+    render() {
+        return ( 
+            <header className = "App-header">
+            <img src = "./logo.png" id = 'logo'alt = "" />
+            </header>
+        );
+    }
+}
+
+
+
+
+
+
+export default Header;

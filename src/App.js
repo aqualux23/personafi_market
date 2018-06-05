bf034650a6575be08e0075a2224b88ec0f3670b9
missmeow71 <sensuousdesigner@gmail.com>
2018-06-05 15:51:07 -0600

    added footer copyright

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Footer from './components/Footer.js'
+
 
 class App extends Component {
   render() {
@@ -7,8 +9,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Personafi</h1>
-          <h2 className = "App-subtitle">A Personality Marketplace</h2>
+          <h2 className ="App-subtitle">A Personality Marketplace</h2>
         </header>
+        <main>
+          
+        </main>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return ( 
+        <footer>
+        <small>&copy; 2018 Personafi.All Rights Reserved.</small>
+        </footer>
+    )
+}
+
+
+export default Footer;
